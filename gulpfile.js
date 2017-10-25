5c89af221205b2e835ccff132ce941278a5a473b
William Goulart <William Goulart>
2017-10-25 19:13:04 -0300

    Updante files, add styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-const config      = require('./config.js');
-const gulp        = require('gulp');
-const sourcemaps  = require('gulp-sourcemaps');
-const babel       = require('gulp-babel');
-const del         = require('del');
-const watch       = require('gulp-watch');
-const sass        = require('gulp-sass');
+const config = require('./config.js');
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const del = require('del');
+const watch = require('gulp-watch');
+const sass = require('gulp-sass');
 const browsersync = require('browser-sync').create();
-const notify      = require('gulp-notify');
+const notify = require('gulp-notify');
 
 gulp.task('browsersync-run', () => {
   browsersync.init(config.browsersync.options);
@@ -17,12 +17,13 @@ gulp.task('js', () => {
   return gulp.src(config.js.src)
     .pipe(sourcemaps.init())
     .pipe(babel())
-    .on('error', notify.onError({ message: 'Error: <%= error.message %>' }))
+    .on('error', notify.onError({
+      message: 'Error: <%= error.message %>'
+    }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.js.dist))
     .pipe(browsersync.stream());
 });
-
 gulp.task('js-watch', ['js'], () => {
   return watch([config.js.src], () => {
     gulp.start(['js']);
@@ -35,7 +36,6 @@ gulp.task('html', () => {
     .pipe(gulp.dest(config.html.dist))
     .pipe(browsersync.stream());
 });
-
 gulp.task('html-watch', ['html'], () => {
   return watch([config.html.src], () => {
     gulp.start(['html']);
@@ -49,7 +49,9 @@ gulp.task('sass', () => {
     .pipe(sass({
       outputStyle: 'compressed',
       includePaths: ['node_modules/susy/sass']
-    }).on('error', sass.logError))
+    }).on('error', error => {
+      console.log(error.message);
+    }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.sass.dist))
     .pipe(browsersync.stream());
